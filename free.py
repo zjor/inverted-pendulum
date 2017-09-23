e1fb8216df55023cac028ae406196e211b67849f
sroyz <sroyz@cisco.com>
2017-09-23 17:33:25 +0200

    interactive simulation

--- free.py
+++ free.py
@@ -2,24 +2,46 @@ import matplotlib.pyplot as plt
 
 from math import sin, pi
 
-# x'' = -x
+'''
+	x'' = -x
 
-h = 0.2
+	x' = y; f(t, x, y) = y
+	y' = -x; g(t, x, y) = -x
+
+'''
+
+h = 0.1
 n = 10000
 
 x = [1.0]
-v = [0.0]
+y = [0.0]
 
 for i in range(n):
 
-	v1 = v[i] - h * x[i]
-	x1 = x[i] + h * v[i]
+	x1 = x[i] + h * y[i]
+	y1 = y[i] - h * x[i]
 
-	v.append(v[i] + h * (-x[i] - x1) / 2.0)
-	x.append(x[i] + h * (v[i] + v1) / 2.0)
+	new_x = x[i] + h * (y[i] + y1) / 2
+	new_y = y[i] - h * (x[i] + x1) / 2
+
+	# k1 = y[i]
+	# k2 = y[i] + k1 / 2
+	# k3 = y[i] + k2 / 2
+	# k4 = y[i] + k3
+	# new_x = x[i] + h * h * (k1 + 2 * k2 + 2 * k3 + k4) / 6
+
+	# k1 = -x[i]
+	# k2 = -x[i] - k1 / 2
+	# k3 = -x[i] - k2 / 2
+	# k4 = -x[i] - k3
+	# new_y = y[i] + h * h * (k1 + 2 * k2 + 2 * k3 + k4) / 6
+
+
+	y.append(new_y)
+	x.append(new_x)
 
 
 
-plt.plot(x, v)
+plt.plot(x, y)
 plt.show()
 
--- simulation/src/main/java/com/github/zjor/App.java
+++ simulation/src/main/java/com/github/zjor/App.java
@@ -1,15 +1,22 @@
 package com.github.zjor;
 
+import lombok.Getter;
+import lombok.Setter;
 import processing.core.PApplet;
+import processing.event.MouseEvent;
 
 public class App extends PApplet {
 
     public static final int WIDTH = 800;
     public static final int HEIGHT = 600;
 
+    static final int CART_WIDTH = 50;
+    static final int CART_HEIGHT = 30;
+
+    static final long TIME_THRESHOLD = 10;
 
     public static final double h = 0.1;
-    public static final double l = 100;
+    public static final double l = 150;
     public static final double gravity = 9.8;
 
     /**
@@ -17,10 +24,7 @@ public class App extends PApplet {
      */
     private double th, dth;
 
-    /**
-     * Mount state variables
-     */
-    private double z, dz;
+    private Cart cart;
 
     @Override
     public void settings() {
@@ -30,11 +34,11 @@ public class App extends PApplet {
     @Override
     public void setup() {
         ellipseMode(CENTER);
-
+        textAlign(LEFT, TOP);
         th = 0.5;
         dth = 0.0;
-        z = WIDTH / 2;
-        dz = 0.0;
+
+        cart = new Cart(WIDTH / 2, 1.0);
     }
 
     @Override
@@ -42,43 +46,129 @@ public class App extends PApplet {
 
         double th1 = th + dth * h;
         double dth1 = dth + dTh(th) * h;
-        double dz1 = dz + dZ(th1) * h;
+
 
         th = th + (dth + dth1) * h / 2;
         dth = dth + (dTh(th) + dTh(th1)) * h / 2;
 
-
-        z = z + (dz + dz1) * h / 2;
-        dz = dz + (dZ(th) + dZ(th1)) * h / 2;
-
+        if (isCartFree) {
+            double dz1 = cart.getV() + dZ(th1) * h;
+            cart.setX(cart.getX() + (cart.getV() + dz1) * h / 2);
+            cart.setV(cart.getV() + (dZ(th) + dZ(th1)) * h / 2);
+        }
 
         fill(255);
         rect(0, 0, WIDTH, HEIGHT);
 
         fill(0);
-        float x0 = WIDTH / 2;
+
         float y0 = HEIGHT / 2;
-        float x1 = (float) (z + l * Math.sin(th));
+        float x1 = (float) (cart.getX() + l * Math.sin(th));
         float y1 = (float) (y0 - l * Math.cos(th));
 
-        ellipse((float) z, y0, 10, 10);
+        line(0, y0, WIDTH, y0);
+        cart.draw();
+        ellipse((float) cart.getX(), y0, 10, 10);
         ellipse(x1, y1, 10, 10);
 
-        line((float) z, y0, x1, y1);
+        line((float) cart.getX(), y0, x1, y1);
 
-        line(0, y0, WIDTH, y0);
+        text("Cart:", 10, 10);
+        text("\tx:" + cart.getX(), 16, 26);
+        text("\tv:" + cart.getV(), 16, 42);
+        text("\ta:" + cart.getA(), 16, 58);
+
+        if (isDragging && !isCartFree) {
+            cart.updateX(mouseX - cart.getX());
+        }
+
+
+    }
+
+    private boolean isDragging;
+    private boolean isCartFree = true;
+
+    private int mouseX;
+
+    @Override
+    public void mousePressed(MouseEvent event) {
+        isDragging = true;
+        mouseX = event.getX();
+        isCartFree = !cart.isInside(event.getX(), event.getY());
+    }
+
+    @Override
+    public void mouseDragged(MouseEvent event) {
+        mouseX = event.getX();
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent event) {
+        isDragging = false;
+        isCartFree = true;
     }
 
     private double dTh(double th) {
-        return (Math.sin(th) * gravity - dZ(th) * Math.cos(th)) / l;
+        if (isCartFree) {
+            return (Math.sin(th) * gravity - dZ(th) * Math.cos(th)) / l;
+        } else {
+            return (Math.sin(th) * gravity - cart.getA() * Math.cos(th)) / l;
+        }
     }
 
     private double dZ(double th) {
-        return - gravity * Math.sin(2.0 * th) / 2;
+        return - 0.2 * gravity * Math.sin(2.0 * th) / 2;
     }
 
     public static void main(String[] args) {
         PApplet.main(App.class.getName());
+    }
+
+    class Cart {
+        private final float y = HEIGHT / 2;
+
+        @Getter
+        @Setter
+        private double x, v, a, m;
+
+        private long time;
+
+        public Cart(double x, double m) {
+            this.x = x;
+            this.m = m;
+            this.v = 0;
+            this.a = 0;
+            time = System.currentTimeMillis();
+        }
+
+        public void draw() {
+            fill(150);
+            rect((float) x - CART_WIDTH / 2, y - CART_HEIGHT / 2, CART_WIDTH, CART_HEIGHT);
+        }
+
+        public boolean isInside(int x, int y) {
+            int x0 = (int) (this.x - CART_WIDTH / 2);
+            int x1 = (int) (this.x + CART_WIDTH / 2);
+            int y0 = (int) (this.y - CART_HEIGHT / 2);
+            int y1 = (int) (this.y + CART_HEIGHT / 2);
+
+            return (x >= x0 && x <= x1 && y >= y0 && y <= y1);
+        }
+
+        public void updateX(double dx) {
+            x += dx;
+
+            long now = System.currentTimeMillis();
+
+            if (now - time > TIME_THRESHOLD) {
+                double dt = 1.0 * (now - time) / 100;
+                a = (dx / dt - v) / dt;
+                v = dx / dt;
+                time = now;
+            }
+
+        }
+
     }
 
 }
