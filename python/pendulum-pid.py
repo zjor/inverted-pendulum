2e7ab869ceeb69bbbb64ef7dd40288c6b91355a3
Sergey Royz <zjor.se@gmail.com>
2019-09-19 13:24:09 +0200

    pendulum: pure PID: wip

--- python/pendulum-pid.py
+++ python/pendulum-pid.py
@@ -1,6 +1,8 @@
+import numpy as np
 import matplotlib.pyplot as plt
 
 from math import sin, cos, pi
+from pidcontroller import PIDController
 
 '''
 	Equations:
@@ -10,52 +12,69 @@ from math import sin, cos, pi
 
 L = 1.0
 g = 9.8
-th = [pi / 12]
-dth = [0.0]
-f = [0.0]
-v = [0.0]
-x = [0.0]
+ths = [-pi / 12]
+ws = [0.0]
+us = [0.0]
+vs = [0.0]
+xs = [0.0]
 x0 = 0.0
 
 dt = 0.01
-N = 10000
+time = np.arange(0, 10, dt)
 
-Kp = 44.0
-Kd = 14.0
+Kp = 10.0
+Kd = 5.0
+Ki = 0.0
 
-xKp = 3.1
-xKd = 4.8
+xKp = 0.0
+xKd = 0.0
+xKi = 0.0
 
-for i in range(0, N):
+pid_th = PIDController(Kp, Kd, Ki, 0.0)
+pid_x = PIDController(xKp, xKd, xKi, 0.0)
 
-	f.append((Kp * th[i] + Kd * dth[i] + xKp * (x[i] - x0) + xKd * v[i]))	
+for i in range(0, len(time) - 1):
 
-	d2th = (g * sin(th[i]) - f[i] * cos(th[i])) / L
-	dth1 = dth[i] + d2th * dt
+	th = ths[-1]
+	w = ws[-1]
+	x = xs[-1]
+	v = vs[-1]
 
-	v.append(v[i] + f[i] * dt)
-	x.append(x[i] + v[i] * dt)
+	# f.append((Kp * th[i] + Kd * dth[i] + xKp * (x[i] - x0) + xKd * v[i]))
+	# u = -pid_th.getControl(th, dt) + pid_x.getControl(x, dt)
+	u = -(Kp * th + Kd * w)
+	print(u)
+
+	us.append(u)	
+
+	d2th = (g * sin(th) - u * cos(th)) / L
+	dth1 = w + d2th * dt
+
+	vs.append(v + u * dt)
+	xs.append(x + v * dt)
 	
-	th.append(th[i] + dth1 * dt)
-	dth.append(dth1)
+	ths.append(th + dth1 * dt)
+	ws.append(dth1)
 
-	if i == 5000:
-		x0 = 0.1
+	# if i == len(time) / 2:
+	# 	x0 = 0.2
 
 plt.figure(1)
 plt.subplot(211)
 plt.title("PD regulator")
-line_th, = plt.plot(range(0, N + 1), th, label = 'Th')
-line_dth, = plt.plot(range(0, N + 1), dth, label = 'dTh')
-line_f, = plt.plot(range(0, N + 1), f, label = 'acceleration')
+line_th, = plt.plot(time, ths, label = 'th')
+line_dth, = plt.plot(time, ws, label = 'w')
 
-plt.legend([line_th, line_dth, line_f])
+plt.legend([line_th, line_dth])
+plt.grid(True)
 
 plt.subplot(212)
-line_v, = plt.plot(range(0, N + 1), v, label = 'v')
-line_x, = plt.plot(range(0, N + 1), x, label = 'x')
+line_v, = plt.plot(time, vs, label = 'v')
+line_x, = plt.plot(time, xs, label = 'x')
+line_u, = plt.plot(time, us, label = 'u')
 
-plt.legend([line_v, line_x])
+plt.legend([line_v, line_x, line_u])
+plt.grid(True)
 plt.show()
 
 
