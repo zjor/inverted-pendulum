a3b50209e1bdc7fe59cda6665568031c00376455
Sergey Royz <zjor.se@gmail.com>
2019-10-08 10:59:41 +0200

    plotting data

--- dc-motor/estimate-params.py
+++ dc-motor/estimate-params.py
@@ -1,3 +1,4 @@
+import re
 import csv
 import glob
 import numpy as np
@@ -6,11 +7,18 @@ import matplotlib.pyplot as pp
 def map_to_float(a):
     return map(lambda x: float(x), a)
 
-def trim_zeroes(data):
-    pass
-
 def normalize_time(data):
-    pass
+    t0 = data[0, 0]
+    for row in data:
+        row[0] = row[0] - t0
+    return data
+
+def get_filenames_and_pwm():
+    result = []
+    for filename in glob.glob("./data/dc-params-pwm-*.csv"):
+        pwm = int(re.search('(\\d+)', filename).group(1))
+        result.append((pwm, filename))
+    return result
 
 def read_params(pwm):
     filename = "./data/dc-params-pwm-%d.csv" % pwm
@@ -21,13 +29,20 @@ def read_params(pwm):
             data.append(map_to_float(row))
     return np.array(data)
 
-# params = read_params(45)
+def read_all():
+    result = {}
+    for datafile in get_filenames_and_pwm():
+        data = read_params(datafile[0])
+        result[datafile[0]] = data
+    return result
 
-# pp.plot(params[:, 0], params[:, 2])
-# pp.show()
+def plot_all(data):
+    for value in data.values():
+        line, = pp.plot(value[:,0], value[:, 2], label=value[0, 1])
+    pp.legend()
+    pp.show()
 
-print glob.glob("./data/dc-params-pwm-*.csv")
-
+plot_all(read_all())    
 # TODO:
 # - plot all data on a single plot
 # - plot set velocity, check linearity
