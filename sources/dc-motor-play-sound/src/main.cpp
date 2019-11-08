a61163c26a0f3a1dc7efa0d201a654dec94f1cf4
Sergey Royz <sergey@ext.latch.com>
2019-11-08 11:35:01 +0100

    make sound with PWM

---
+++ sources/dc-motor-play-sound/src/main.cpp
@@ -0,0 +1,62 @@
+/**
+ * Plays sound using different PWM frequencies and a DC motor
+ * Frequencies
+ * TCCR2B = TCCR2B & B11111000 | B00000001;    // set timer 2 divisor to     1 for PWM frequency of 31372.55 Hz
+ * TCCR2B = TCCR2B & B11111000 | B00000010;    // set timer 2 divisor to     8 for PWM frequency of  3921.16 Hz
+ * TCCR2B = TCCR2B & B11111000 | B00000011;    // set timer 2 divisor to    32 for PWM frequency of   980.39 Hz
+ * TCCR2B = TCCR2B & B11111000 | B00000100;    // set timer 2 divisor to    64 for PWM frequency of   490.20 Hz
+ * TCCR2B = TCCR2B & B11111000 | B00000101;    // set timer 2 divisor to   128 for PWM frequency of   245.10 Hz
+ * TCCR2B = TCCR2B & B11111000 | B00000110;    // set timer 2 divisor to   256 for PWM frequency of   122.55 Hz
+ * TCCR2B = TCCR2B & B11111000 | B00000111;    // set timer 2 divisor to  1024 for PWM frequency of    30.64 Hz
+ * Source: https://arduinoinfo.mywikis.net/wiki/Arduino-PWM-Frequency
+ */
+
+#include <Arduino.h>
+
+#define PWM_PIN 10
+#define DIR_PIN 8
+
+int dir = HIGH;
+unsigned int i = 0;
+
+void setup() {
+  pinMode(PWM_PIN, OUTPUT);
+  pinMode(DIR_PIN, OUTPUT);
+}
+
+void loop() {
+
+  switch (i % 6) {
+    case 0:
+      TCCR2B = TCCR2B & B11111000 | B00000001;    // set timer 2 divisor to     1 for PWM frequency of 31372.55 Hz
+      break;  
+    case 1:
+      TCCR2B = TCCR2B & B11111000 | B00000010;    // set timer 2 divisor to     8 for PWM frequency of  3921.16 Hz
+      break;
+    case 2:
+      TCCR2B = TCCR2B & B11111000 | B00000011;    // set timer 2 divisor to    32 for PWM frequency of   980.39 Hz
+      break;
+    case 3:
+      TCCR2B = TCCR2B & B11111000 | B00000100;    // set timer 2 divisor to    64 for PWM frequency of   490.20 Hz
+      break;
+    case 4:
+      TCCR2B = TCCR2B & B11111000 | B00000101;    // set timer 2 divisor to   128 for PWM frequency of   245.10 Hz
+      break;
+    case 5:
+      TCCR2B = TCCR2B & B11111000 | B00000110;    // set timer 2 divisor to   256 for PWM frequency of   122.55 Hz
+      break;
+    case 6:
+      TCCR2B = TCCR2B & B11111000 | B00000111;    // set timer 2 divisor to  1024 for PWM frequency of    30.64 Hz
+      break;
+  }
+  i++;
+
+  // should not be enough to move the motor
+  analogWrite(PWM_PIN, 20);
+  digitalWrite(DIR_PIN, dir);
+  dir = !dir;
+  delay(500);
+  digitalWrite(DIR_PIN, dir);
+  delay(500);  
+
+}
