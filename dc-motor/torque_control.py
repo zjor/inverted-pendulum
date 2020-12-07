171d9f20c9783c52db91cb6da4e1c6699ee02a5c
Sergey Royz <sergey@ext.latch.com>
2020-12-07 18:52:36 +0100

    integrate manually

--- dc-motor/torque_control.py
+++ dc-motor/torque_control.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as pp
 import scipy.integrate as integrate
 
 times = np.linspace(0, 10, 1000)
+dt = times[1] - times[0]
 
 X0 = 1.0
 V0 = 0.0
@@ -10,22 +11,24 @@ V0 = 0.0
 x_Kp = 5.0
 v_Kp = 1.0
 
-state = np.array([X0, V0])
+state = [[X0, V0]]
 
 
-def derivatives(state_, t):
-    x, v = state_
-    u = -x_Kp * x - v_Kp * v
-    ds = np.zeros_like(state_)
-    ds[0] = v
-    ds[1] = u
-    return ds
+def integrate():
+    x_, v_ = state[-1]
+    u = -x_Kp * x_ - v_Kp * v_
+    state.append([x_ + v_ * dt, v_ + u * dt])
 
 
-solution = integrate.odeint(derivatives, state, times)
+for i in range(len(times)):
+    integrate()
 
-pp.plot(times, solution[:, 0], label="X")
-pp.plot(times, solution[:, 1], label="V")
-pp.legend()
+state = np.array(state)
+X = state[:, 0]
+V = state[:, 1]
+
+pp.plot(times, X[:-1])
+pp.plot(times, V[:-1])
 pp.grid(True)
 pp.show()
+
