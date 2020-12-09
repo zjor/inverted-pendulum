a31ea33a7da8b66b81eca29f31cd90de835d7ba3
Sergey Royz <sergey@ext.latch.com>
2020-12-09 23:07:27 +0100

    universal solver

---
+++ dc-motor/pid.py
@@ -0,0 +1,16 @@
+class PID:
+    def __init__(self, target, dt, k_p=0.5, k_i=0.0, k_d=0):
+        self.target = target
+        self.dt = dt
+        self.k_p = k_p
+        self.k_i = k_i
+        self.k_d = k_d
+        self.last_value = None
+        self.err_acc = 0.0
+
+    def get_control(self, value):
+        e = self.target - value
+        de = - ((value - self.last_value) / self.dt) if self.last_value else 0.0
+        self.err_acc += self.k_i * e * self.dt
+        self.last_value = e
+        return self.k_p * e + self.err_acc + self.k_d * de
--- dc-motor/torque_control.py
+++ dc-motor/torque_control.py
@@ -1,6 +1,8 @@
 import matplotlib.pyplot as pp
 import numpy as np
 
+from pid import PID
+
 times = np.linspace(0, 20, 1000)
 dt = times[1] - times[0]
 
@@ -12,46 +14,27 @@ K2 = 1.0
 B = 0.1
 J = 2.0
 
-state = [[0, 0]]
-
-
-class PID:
-    def __init__(self, target, dt, k_p=0.5, k_i=0.0, k_d=0):
-        self.target = target
-        self.dt = dt
-        self.k_p = k_p
-        self.k_i = k_i
-        self.k_d = k_d
-        self.last_value = None
-        self.err_acc = 0.0
-
-    def get_control(self, value):
-        e = self.target - value
-        de = - ((value - self.last_value) / dt) if self.last_value else 0.0
-        self.err_acc += self.k_i * e * dt
-        self.last_value = e
-        return self.k_p * e + self.err_acc + self.k_d * de
-
+state = [[1.0, 0]]
 
 pid = PID(1.0, dt, k_p=0.8, k_i=0.2, k_d=0.004)
 
 
-def derivatives(state_, u):
+def derivatives(state_):
+    u = 3.0
     i_, w_ = state_
     di = (-R * i - K2 * w_ + u) / L
     dw = (K1 * i - B * w_) / J
     return [di, dw]
 
 
-def integrate():
-    i_, w_ = state[-1]
-    di, dw = derivatives(state[-1], 3.0)
-    print(di, dw)
-    state.append([i_ + di * dt, w_ + dw * dt])
+def integrate(state_, dt_, derivatives_func):
+    k1 = derivatives_func(state_)
+    k2 = derivatives_func([v + d * dt_ for v, d in zip(state_, k1)])
+    return [v + (k1_ + k2_) * dt_ / 2 for v, k1_, k2_ in zip(state_, k1, k2)]
 
 
 for i in range(len(times)):
-    integrate()
+    state.append(integrate(state[-1], dt, derivatives))
 
 state = np.array(state)
 current = state[:, 0]
