5e371faafbf2b6c4b01100710bffbf0e9f8dacbe
Sergey Royz <sergey@ext.latch.com>
2020-12-10 11:26:49 +0100

    position control

--- dc-motor/pid.py
+++ dc-motor/pid.py
@@ -1,5 +1,5 @@
 class PID:
-    def __init__(self, target, dt, k_p=0.5, k_i=0.0, k_d=0):
+    def __init__(self, target, dt, k_p=0.5, k_i=0.0, k_d=0, min_value=None, max_value=None):
         self.target = target
         self.dt = dt
         self.k_p = k_p
@@ -7,13 +7,28 @@ class PID:
         self.k_d = k_d
         self.last_value = None
         self.err_acc = 0.0
+        self.min_value = min_value
+        self.max_value = max_value
+
+    def clamp_min(self, value):
+        if self.min_value:
+            return max(value, self.min_value)
+        else:
+            return value
+
+    def clamp_max(self, value):
+        if self.max_value:
+            return min(value, self.max_value)
+        else:
+            return value
 
     def get_control(self, value):
         e = self.target - value
         de = - ((value - self.last_value) / self.dt) if self.last_value else 0.0
         self.err_acc += self.k_i * e * self.dt
         self.last_value = e
-        return self.k_p * e + self.err_acc + self.k_d * de
+        u = self.k_p * e + self.err_acc + self.k_d * de
+        return self.clamp_max(self.clamp_min(u))
 
     def set_target(self, new_target):
         self.target = new_target
--- dc-motor/torque_control.py
+++ dc-motor/torque_control.py
@@ -3,7 +3,8 @@ import numpy as np
 
 from pid import PID
 
-times = np.linspace(0, 5, 3000)
+times = np.linspace(0, 15, 3000)
+dt = times[1] - times[0]
 
 # Motor params
 R = 4.5
@@ -13,14 +14,15 @@ K2 = 1.0
 B = 0.1
 J = 2.0
 
-state = [[0, 0]]
 u_log = [0]
 i_log = []
 
-pid = PID(target=0.0, dt=(times[1] - times[0]), k_p=1.1, k_i=1.2, k_d=0.0)
-pid_w = PID(target=1.0, dt=(times[1] - times[0]), k_p=10.0, k_i=0.1, k_d=0.0)
+pid = PID(target=0.0, dt=dt, k_p=1.5, k_i=1.2, k_d=0.003, min_value=-12, max_value=12)
+pid_w = PID(target=0.0, dt=dt, k_p=4.0, k_i=0.2, k_d=0.01)
+pid_x = PID(target=2.0, dt=dt, k_p=5.0, k_i=0.2, k_d=0.01)
 
 
+# deprecated as we neglect the inductance L
 def derivatives_inductance(state_, t_, dt_):
     i_, w_ = state_
     u = pid.get_control(i_)
@@ -32,17 +34,22 @@ def derivatives_inductance(state_, t_, dt_):
 
 
 def derivatives_no_inductance(state_, t_, dt_):
-    w_ = state_[0]
+    x_, w_ = state_
     u = u_log[-1]
     i_ = u / R - K2 / R * w_
     i_log.append(i_)
+    x_control = pid_x.get_control(x_)
+    pid_w.set_target(x_control)
 
     w_control = pid_w.get_control(w_)
     pid.set_target(w_control)
-    control = pid.get_control(i_)
-    u_log.append(control)
+
+    i_control = pid.get_control(i_)
+
+    u_log.append(i_control)
     dw = (K1 * i_ - B * w_) / J
-    return [dw]
+    dx = w_
+    return [dx, dw]
 
 
 def integrate(state_, t_, dt_, derivatives_func):
@@ -52,18 +59,18 @@ def integrate(state_, t_, dt_, derivatives_func):
 
 
 def solve(initial_state_, times_, integrate_func, derivative_func):
-    dt = times_[1] - times_[0]
+    dt_ = times_[1] - times_[0]
     states_ = [initial_state_]
     for t in times_:
-        states_.append(integrate_func(states_[-1], t, dt, derivative_func))
+        states_.append(integrate_func(states_[-1], t, dt_, derivative_func))
     return np.array(states_)
 
 
-state = solve([0.0], times, integrate, derivatives_no_inductance)
-# current = state[:, 0]
-velocity = state[:, 0]
+state = solve([0.0, 0.0], times, integrate, derivatives_no_inductance)
+position = state[:, 0]
+velocity = state[:, 1]
 
-# pp.plot(times, current[:-1], label="i")
+pp.plot(times, position[:-1], label="x")
 pp.plot(times, velocity[:-1], label="w")
 pp.plot(times, u_log[::2][:-1], label="U")
 pp.plot(times, i_log[::2], label="i")
